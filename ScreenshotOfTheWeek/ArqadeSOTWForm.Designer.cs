6688dc00285f8211e259ce9e0e7d9d9fae1db667
Bryan Cave <bryan.d.cave@gmail.com>
2024-10-15 07:21:55 -0300

    Initial Commit

---
+++ ScreenshotOfTheWeek/ArqadeSOTWForm.Designer.cs
@@ -0,0 +1,281 @@
+﻿namespace ScreenshotOfTheWeek
+{
+    partial class ArqadeSOTWForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            copyToClipboard = new Button();
+            sotwNumberTextBox = new TextBox();
+            sotwNumberLabel = new Label();
+            themeTitleTextBox = new TextBox();
+            themeTitleLabel = new Label();
+            themeBodyLabel = new Label();
+            themeBodyRichTextBox = new RichTextBox();
+            label1 = new Label();
+            prevPostTextBox = new TextBox();
+            label2 = new Label();
+            label3 = new Label();
+            winningPostTextBox = new TextBox();
+            label4 = new Label();
+            gameTagTextBox = new TextBox();
+            label5 = new Label();
+            winningUserTextBox = new TextBox();
+            label6 = new Label();
+            winningScreenshotTextBox = new TextBox();
+            label7 = new Label();
+            winningUpvotesTextBox = new TextBox();
+            SuspendLayout();
+            // 
+            // copyToClipboard
+            // 
+            copyToClipboard.Location = new Point(283, 405);
+            copyToClipboard.Name = "copyToClipboard";
+            copyToClipboard.Size = new Size(135, 23);
+            copyToClipboard.TabIndex = 0;
+            copyToClipboard.Text = "Copy to Clipboard";
+            copyToClipboard.UseVisualStyleBackColor = true;
+            copyToClipboard.Click += copyToClickboard_Click;
+            // 
+            // sotwNumberTextBox
+            // 
+            sotwNumberTextBox.Location = new Point(103, 37);
+            sotwNumberTextBox.Name = "sotwNumberTextBox";
+            sotwNumberTextBox.Size = new Size(145, 23);
+            sotwNumberTextBox.TabIndex = 1;
+            sotwNumberTextBox.TextChanged += sotwNumberTextBox_TextChanged;
+            // 
+            // sotwNumberLabel
+            // 
+            sotwNumberLabel.AutoSize = true;
+            sotwNumberLabel.Location = new Point(12, 40);
+            sotwNumberLabel.Name = "sotwNumberLabel";
+            sotwNumberLabel.Size = new Size(85, 15);
+            sotwNumberLabel.TabIndex = 2;
+            sotwNumberLabel.Text = "SOTW Number";
+            // 
+            // themeTitleTextBox
+            // 
+            themeTitleTextBox.Location = new Point(103, 66);
+            themeTitleTextBox.Name = "themeTitleTextBox";
+            themeTitleTextBox.Size = new Size(145, 23);
+            themeTitleTextBox.TabIndex = 3;
+            themeTitleTextBox.Text = "No Theme";
+            // 
+            // themeTitleLabel
+            // 
+            themeTitleLabel.AutoSize = true;
+            themeTitleLabel.Location = new Point(12, 69);
+            themeTitleLabel.Name = "themeTitleLabel";
+            themeTitleLabel.Size = new Size(68, 15);
+            themeTitleLabel.TabIndex = 4;
+            themeTitleLabel.Text = "Theme Title";
+            // 
+            // themeBodyLabel
+            // 
+            themeBodyLabel.AutoSize = true;
+            themeBodyLabel.Location = new Point(12, 100);
+            themeBodyLabel.Name = "themeBodyLabel";
+            themeBodyLabel.Size = new Size(73, 15);
+            themeBodyLabel.TabIndex = 5;
+            themeBodyLabel.Text = "Theme Body";
+            // 
+            // themeBodyRichTextBox
+            // 
+            themeBodyRichTextBox.Location = new Point(103, 95);
+            themeBodyRichTextBox.Name = "themeBodyRichTextBox";
+            themeBodyRichTextBox.Size = new Size(315, 104);
+            themeBodyRichTextBox.TabIndex = 6;
+            themeBodyRichTextBox.Text = "There is no theme this week.";
+            themeBodyRichTextBox.TextChanged += themeBodyRichTextBox_TextChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(2, 208);
+            label1.Name = "label1";
+            label1.Size = new Size(126, 15);
+            label1.TabIndex = 7;
+            label1.Text = "Previous SOTW Post ID";
+            // 
+            // prevPostTextBox
+            // 
+            prevPostTextBox.Location = new Point(134, 205);
+            prevPostTextBox.Name = "prevPostTextBox";
+            prevPostTextBox.Size = new Size(84, 23);
+            prevPostTextBox.TabIndex = 8;
+            prevPostTextBox.TextChanged += prevPostTextBox_TextChanged;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(12, 9);
+            label2.Name = "label2";
+            label2.Size = new Size(232, 25);
+            label2.TabIndex = 9;
+            label2.Text = "Arqade SOTW Generator";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(235, 208);
+            label3.Name = "label3";
+            label3.Size = new Size(92, 15);
+            label3.TabIndex = 10;
+            label3.Text = "Winning Post ID";
+            // 
+            // winningPostTextBox
+            // 
+            winningPostTextBox.Location = new Point(333, 205);
+            winningPostTextBox.Name = "winningPostTextBox";
+            winningPostTextBox.Size = new Size(85, 23);
+            winningPostTextBox.TabIndex = 11;
+            winningPostTextBox.TextChanged += winningPostTextBox_TextChanged;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(12, 239);
+            label4.Name = "label4";
+            label4.Size = new Size(62, 15);
+            label4.TabIndex = 12;
+            label4.Text = "Game  Tag";
+            // 
+            // gameTagTextBox
+            // 
+            gameTagTextBox.Location = new Point(80, 236);
+            gameTagTextBox.Name = "gameTagTextBox";
+            gameTagTextBox.Size = new Size(338, 23);
+            gameTagTextBox.TabIndex = 13;
+            gameTagTextBox.TextChanged += gameTagTextBox_TextChanged;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(2, 271);
+            label5.Name = "label5";
+            label5.Size = new Size(78, 15);
+            label5.TabIndex = 14;
+            label5.Text = "Winning User";
+            // 
+            // winningUserTextBox
+            // 
+            winningUserTextBox.Location = new Point(80, 268);
+            winningUserTextBox.Name = "winningUserTextBox";
+            winningUserTextBox.Size = new Size(338, 23);
+            winningUserTextBox.TabIndex = 15;
+            winningUserTextBox.TextChanged += winningUserTextBox_TextChanged;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(2, 303);
+            label6.Name = "label6";
+            label6.Size = new Size(113, 15);
+            label6.TabIndex = 16;
+            label6.Text = "Winning Screenshot";
+            // 
+            // winningScreenshotTextBox
+            // 
+            winningScreenshotTextBox.Location = new Point(134, 300);
+            winningScreenshotTextBox.Name = "winningScreenshotTextBox";
+            winningScreenshotTextBox.Size = new Size(284, 23);
+            winningScreenshotTextBox.TabIndex = 17;
+            winningScreenshotTextBox.TextChanged += winningScreenshotTextBox_TextChanged;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(2, 330);
+            label7.Name = "label7";
+            label7.Size = new Size(124, 15);
+            label7.TabIndex = 18;
+            label7.Text = "Winning Post Upvotes";
+            // 
+            // winningUpvotesTextBox
+            // 
+            winningUpvotesTextBox.Location = new Point(134, 327);
+            winningUpvotesTextBox.Name = "winningUpvotesTextBox";
+            winningUpvotesTextBox.Size = new Size(57, 23);
+            winningUpvotesTextBox.TabIndex = 19;
+            winningUpvotesTextBox.TextChanged += winningUpvotesTextBox_TextChanged;
+            // 
+            // ArqadeSOTWForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(438, 450);
+            Controls.Add(winningUpvotesTextBox);
+            Controls.Add(label7);
+            Controls.Add(winningScreenshotTextBox);
+            Controls.Add(label6);
+            Controls.Add(winningUserTextBox);
+            Controls.Add(label5);
+            Controls.Add(gameTagTextBox);
+            Controls.Add(label4);
+            Controls.Add(winningPostTextBox);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(prevPostTextBox);
+            Controls.Add(label1);
+            Controls.Add(themeBodyRichTextBox);
+            Controls.Add(themeBodyLabel);
+            Controls.Add(themeTitleLabel);
+            Controls.Add(themeTitleTextBox);
+            Controls.Add(sotwNumberLabel);
+            Controls.Add(sotwNumberTextBox);
+            Controls.Add(copyToClipboard);
+            Name = "ArqadeSOTWForm";
+            Text = "Arqade SOTW Generator";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button copyToClipboard;
+        private TextBox sotwNumberTextBox;
+        private Label sotwNumberLabel;
+        private TextBox themeTitleTextBox;
+        private Label themeTitleLabel;
+        private Label themeBodyLabel;
+        private RichTextBox themeBodyRichTextBox;
+        private Label label1;
+        private TextBox prevPostTextBox;
+        private Label label2;
+        private Label label3;
+        private TextBox winningPostTextBox;
+        private Label label4;
+        private TextBox gameTagTextBox;
+        private Label label5;
+        private TextBox winningUserTextBox;
+        private Label label6;
+        private TextBox winningScreenshotTextBox;
+        private Label label7;
+        private TextBox winningUpvotesTextBox;
+    }
+}
---
+++ ScreenshotOfTheWeek/ArqadeSOTWForm.cs
@@ -0,0 +1,139 @@
+using System.Windows.Forms.Design;
+
+namespace ScreenshotOfTheWeek
+{
+    public partial class ArqadeSOTWForm : Form
+    {
+        private int SOTWNumber { get; set; }
+
+        private string ThemeTitle { get; set; } = "No theme";
+
+        private string ThemeText { get; set; } = "There is no theme this week.";
+
+        private string Winner { get; set; }
+
+        private string GameTag { get; set; }
+
+        private int Upvotes { get; set; }
+
+        private int PreviousPostId { get; set; }
+
+        private int WinningPostId { get; set; }
+
+        private string WinningScreenshot { get; set; }
+
+        private string ThemeFull
+        {
+            get
+            {
+                return $"\r\n\r\n# {ThemeTitle}" +
+                    $"\r\n\r\n{ThemeText}";
+            }
+        }
+
+        private string SOTWNumberWithPostFix
+        {
+            get
+            {
+                var lastDigit = SOTWNumber % 10;
+                var postFix = "th";
+                if (lastDigit == 2)
+                    postFix = "nd";
+                else if (lastDigit == 3)
+                    postFix = "rd";
+                else if (lastDigit == 4 || lastDigit == 5 || lastDigit == 6
+                    || lastDigit == 7 || lastDigit == 8 || lastDigit == 9)
+                    postFix = "th";
+
+                return $"{SOTWNumber}<sup>{postFix}</sup>";
+            }
+        }
+
+        public ArqadeSOTWForm()
+        {
+            InitializeComponent();
+        }
+
+        private void copyToClickboard_Click(object sender, EventArgs e)
+        {
+            var closeDate = DateTime.Now.AddDays(7).ToLongDateString();
+            var finishDate = DateTime.Now.AddDays(14).ToLongDateString();
+
+            var template = $"Hello and welcome to the **{SOTWNumberWithPostFix}** edition of the Screenshot of the Week!" +
+                $"\r\n\r\nTo start with, congratulations to the winner of the previous contest! [{Winner}][winning post]'s screenshot from [tag:{GameTag}] won with {Upvotes} upvotes! " +
+                "\r\n\r\n[![Last week's winning screenshot one][winning screenshot]][winning screenshot]" +
+                "\r\n\r\n---" +
+                "\r\n\r\nTo submit a screenshot, simply post it as an answer to this question, but please take note of the following:" +
+                "\r\n\r\n- One screenshot per post, and one post per person, please!" +
+                "\r\n- Limited picture modifications are allowed, such as cropping, simple filters, and blurring of identifying info such as names, but not adding other images or text. " +
+                "\r\n- To ensure a fair playing field, please post screenshots you've taken yourself rather than ones you found online. " +
+                "\r\n- Stack Exchange’s [Code of Conduct][code of conduct] still applies - so if it would be unacceptable to post normally, it’s unacceptable here. " +
+                "\r\n\r\nAlso, try to avoid pictures that include spoilers. We want everyone to be able to enjoy this contest, so the less spoilers the better." +
+                $"\r\n\r\n### How long will the contest run?\r\nWe will accept submissions for a week, until {closeDate}, then have a second week-long period where only voting will be accepted. This is to give every submission, even those posted at the end of the first week, a chance to be voted on by everyone. " +
+                $"\r\n\r\nAfter the end of the second week, at {finishDate}, we will count upvotes only to determine the winning screenshot, which will be featured for a week on the main site's photo widget." +
+                "\r\n\r\n------------------------" +
+                $"{ThemeFull}" +
+                "\r\n\r\nAs a reminder, we're always accepting suggestions for themed weeks, and have compiled that suggestion process into a [question of its own][themes collection].  Additionally, there is the [hall of fame][hall of fame] you can check out that contains all the previous contest winners." +
+                $"\r\n\r\n  [contest prev]:       //meta.arqade.com/q/{PreviousPostId}" +
+                "\r\n  [contest next]:       //meta.arqade.com/q/17226" +
+                $"\r\n\r\n  [winning post]:       https://gaming.meta.stackexchange.com/a/{PreviousPostId}/{WinningPostId}" +
+                $"\r\n  [winning screenshot]: {WinningScreenshot}" +
+                "\r\n\r\n  [code of conduct]:   //arqade.com/conduct" +
+                "\r\n  [themes collection]: //meta.arqade.com/q/15029" +
+                "\r\n  [hall of fame]:      //meta.arqade.com/q/14939";
+            Clipboard.SetText(template);
+        }
+
+        private void sotwNumberTextBox_TextChanged(object sender, EventArgs e)
+        {
+            var textBox = sender as TextBox ?? new TextBox();
+            if (int.TryParse(textBox.Text, out int value))
+                SOTWNumber = value;
+        }
+
+        private void themeBodyRichTextBox_TextChanged(object sender, EventArgs e)
+        {
+            var textBox = sender as RichTextBox ?? new RichTextBox();
+            ThemeText = textBox.Text;
+        }
+
+        private void prevPostTextBox_TextChanged(object sender, EventArgs e)
+        {
+            var textBox = sender as TextBox ?? new TextBox();
+            if (int.TryParse(textBox.Text, out int value))
+                PreviousPostId = value;
+        }
+
+        private void winningPostTextBox_TextChanged(object sender, EventArgs e)
+        {
+            var textBox = sender as TextBox ?? new TextBox();
+            if (int.TryParse(textBox.Text, out int value))
+                WinningPostId = value;
+        }
+
+        private void gameTagTextBox_TextChanged(object sender, EventArgs e)
+        {
+            var textBox = sender as TextBox ?? new TextBox();
+            GameTag = textBox.Text;
+        }
+
+        private void winningUserTextBox_TextChanged(object sender, EventArgs e)
+        {
+            var textBox = sender as TextBox ?? new TextBox();
+            Winner = textBox.Text;
+        }
+
+        private void winningScreenshotTextBox_TextChanged(object sender, EventArgs e)
+        {
+            var textBox = sender as TextBox ?? new TextBox();
+            WinningScreenshot = textBox.Text;
+        }
+
+        private void winningUpvotesTextBox_TextChanged(object sender, EventArgs e)
+        {
+            var textBox = sender as TextBox ?? new TextBox();
+            if (int.TryParse(textBox.Text, out int value))
+                Upvotes = value;
+        }
+    }
+}
